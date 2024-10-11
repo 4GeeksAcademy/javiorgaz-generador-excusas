33a28d640594a14ee516c42c58ac8b97e0a22fa7
javiorgaz <javi.orgaz00@gmail.com>
2024-10-11 09:57:52 +0000

    generado excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let primera = "Un ";
+  let nombre = [
+    "mapache ",
+    "cocodrilo ",
+    "escarabajo ",
+    "gato ",
+    "funcionario "
+  ];
+  let adjetivo = [
+    "furioso ",
+    "bipedo ",
+    "sudado ",
+    "asustado ",
+    "feliz ",
+    "viejo ",
+    "bipedo "
+  ];
+  let accion = [
+    "ha lanzado ",
+    "se ha comido ",
+    "ha vendido ",
+    "ha escupido ",
+    "me ha robado ",
+    "ha quemado "
+  ];
+  let objeto = [
+    "mi raqueta ",
+    "mi chaqueta ",
+    "mi telefono ",
+    "mi libro ",
+    "mi comida "
+  ];
+  let donde = ["al lago", "a una central nuclear", "en mi casa"];
+
+  let primeraN = Math.floor(Math.random() * primera.length);
+  let nombreN = Math.floor(Math.random() * nombre.length);
+  let adjetivoN = Math.floor(Math.random() * adjetivo.length);
+  let accionN = Math.floor(Math.random() * accion.length);
+  let objetoN = Math.floor(Math.random() * objeto.length);
+  let dondeN = Math.floor(Math.random() * donde.length);
+
+  document.querySelector("#excusa").innerHTML =
+    primera +
+    nombre[primeraN] +
+    adjetivo[adjetivoN] +
+    accion[accionN] +
+    objeto[objetoN] +
+    donde[dondeN];
 };
